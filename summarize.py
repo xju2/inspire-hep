7a337f861f2bccb3646cd4e2f352900b814c88fa
Xiangyang Ju <xiangyang.ju@gmail.com>
2022-04-15 17:11:33 -0700

    up

---
+++ summarize.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""Summarize your publications
+"""
+
+if __name__ == '__main__':
+    import os
+    import pandas as pd
+    import argparse
+    parser = argparse.ArgumentParser(description='Summarize your publications')
+    add_arg = parser.add_argument
+    add_arg('-i', '--input', help='input file', default='publications/mypub.csv')
+    
+    args = parser.parse_args()
+
+    if os.path.exists(args.input):
+        df = pd.read_csv(args.input)
+    else:
+        exit(1)
+
+    tot_pub = df.shape[0]
+    tot_citation= df['citation_count'].sum()
+    print("Total {:,} publications with {:,} of citations".format(
+        tot_pub, tot_citation))
+    
